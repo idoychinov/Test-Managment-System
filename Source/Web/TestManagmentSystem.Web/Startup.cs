1cfb48cef52296bd2d477509fadb40d92e92625b
idoychinov <doychinov.ivan@gmail.com>
2014-11-12 20:00:46 +0200

    Directory structure and web project added

---
+++ Source/Web/TestManagmentSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestManagmentSystem.Web.Startup))]
+namespace TestManagmentSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
