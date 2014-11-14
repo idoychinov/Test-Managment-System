bfc9b9e1a2cccac6104dd89e81a3030a3bfdc487
idoychinov <doychinov.ivan@gmail.com>
2014-11-15 00:25:44 +0200

    Moved ApplicationUser and DbContext

--- Source/Web/TestManagmentSystem.Web/App_Start/Startup.Auth.cs
+++ Source/Web/TestManagmentSystem.Web/App_Start/Startup.Auth.cs
@@ -5,6 +5,8 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
+using TestManagmentSystem.Data;
+using TestManagmentSystem.Data.Models;
 using TestManagmentSystem.Web.Models;
 
 namespace TestManagmentSystem.Web
