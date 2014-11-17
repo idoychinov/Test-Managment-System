214e6c657babcfd83db8265448e01829849b9312
idoychinov <doychinov.ivan@gmail.com>
2014-11-18 01:49:03 +0200

    systems administration added

--- Source/Web/TestManagmentSystem.Web/Areas/Administration/Controllers/TestedSystemsController.cs
+++ Source/Web/TestManagmentSystem.Web/Areas/Administration/Controllers/TestedSystemsController.cs
@@ -1,14 +1,19 @@
 ﻿namespace TestManagmentSystem.Web.Areas.Administration.Controllers
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using AutoMapper.QueryableExtensions;
+    using Kendo.Mvc.UI;
     using TestManagmentSystem.Data.UnitOfWork;
     using TestManagmentSystem.Web.Areas.Administration.Controllers.Base;
+    using Model = TestManagmentSystem.Data.Models.TestedSystem;
+    using ViewModel = TestManagmentSystem.Web.Areas.Administration.ViewModels.TestedSystems.TestedSystemViewModel;
 
-    public class TestedSystemsController : AdminController
+    public class TestedSystemsController : KendoGridAdministrationController
     {
         public TestedSystemsController(ITestManagmentSystemData data)
             :base(data)
@@ -19,5 +24,69 @@
         {
             return View();
         }
+
+        protected override IEnumerable GetData()
+        {
+            return this.Data
+                .TestedSystems
+                .All()
+                .Project()
+                .To<ViewModel>();
+        }
+
+        protected override T GetById<T>(object id)
+        {
+            return this.Data.TestedSystems.GetById(id) as T;
+        }
+
+        [HttpPost]
+        public ActionResult Create([DataSourceRequest]DataSourceRequest request, ViewModel model)
+        {
+            var dbModel = base.Create<Model>(model);
+            if (dbModel != null) model.Id = dbModel.Id;
+            return this.GridOperation(model, request);
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, ViewModel model)
+        {
+            base.Update<Model, ViewModel>(model, model.Id);
+            return this.GridOperation(model, request);
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, ViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var testedSystem = this.Data.TestedSystems.GetById(model.Id.Value);
+
+                var projects = this.Data
+                        .Projects
+                        .All()
+                        .Where(p => p.TestedSystemId == testedSystem.Id)
+                        .Select(c => c.Id)
+                        .ToList();
+                foreach(var project in projects)
+                {
+                    this.Data.Projects.Delete(project);
+                }
+
+                this.Data.SaveChanges();
+
+                foreach (var environment in testedSystem.Environments.Select(t => t.Id).ToList())
+                {
+                    this.Data.Environments.Delete(environment);
+                }
+
+                this.Data.SaveChanges();
+
+                this.Data.TestedSystems.Delete(testedSystem);
+                this.Data.SaveChanges();
+            }
+
+            return this.GridOperation(model, request);
+        }
+
     }
 }
---
+++ Source/Web/TestManagmentSystem.Web/Areas/Administration/ViewModels/TestedSystems/TestedSystemViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestManagmentSystem.Web.Areas.Administration.ViewModels.TestedSystems
+{
+
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using TestManagmentSystem.Data.Models;
+    using TestManagmentSystem.Web.Infrastructure.Mapping;
+
+    public class TestedSystemViewModel : IMapFrom<TestedSystem>
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int? Id { get; set; }
+
+        [Required]
+        [UIHint("String")]
+        public string Name { get; set; }
+
+        [UIHint("MultiLineText")]
+        public string Description { get; set; }
+    }
+}
